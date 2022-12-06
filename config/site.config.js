3665742f86f8e7c6a369d60cd4bfa7a421582319
aiyamia <2272682098@qq.com>
2022-12-06 23:00:06 +0800

    add pass

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/mosh的python课程（付费资源）'],
+  protectedRoutes: ['/mosh的python课程（付费资源）','/学术讲座','/公号会员学术大礼包'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
